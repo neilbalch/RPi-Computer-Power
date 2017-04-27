47e25f8d24febc515fd8837a43ec25e63524661e
Neil Balch <neilbalch@gmail.com>
2017-04-27 10:35:37 -0700

    Added Python Scripts
    As of July 2016

---
+++ GPIO Python Scripts/start.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as pins
+import time
+
+
+pins.setmode(GPIO.BOARD)
+pins.setup(7, GPIO.OUT)   # Power button
+#pins.setup(11, GPIO.OUT) # Reset button
+
+pins.output(7, True)
+time.sleep(1)
+pins.output(7, False)
+pins.cleanup()
