7bebc1469edfc11e9f0a77bd9d7ccb570f32feb0
Neil Balch <neilbalch@gmail.com>
2017-04-29 13:39:29 -0700

    Fixed hashing so it actually works

--- RPi Server/routes/index.js
+++ RPi Server/routes/index.js
@@ -30,7 +30,6 @@ router.get('/', function(req, res, next) {
   }
 });
 
-// Login Screen
 router.post('/login',function(req,res,next){
   console.log((new Date()).getTime() - lastAttempt.getTime());
   if((new Date()).getTime() - lastAttempt.getTime() < 10000){// make sure you cannot enter a password (invalid or not) within 6 seconds of your last invalid one
@@ -55,13 +54,11 @@ router.post('/login',function(req,res,next){
   });
 });
 
-// Logout Screen
 router.post('/logout',function(req,res,next){
   req.session.signedin = undefined;
   res.redirect('/');
 });
 
-// Program Output Screen
 router.post('/:script', function(req, res, next) {
 //  bcrypt.compare(req.body.password,"$2a$10$XlQowFGQZD8yUpBM3PlKC.vbQV4xHXqkQ8XDAkI3JdQeUl5dk9INi",function(err,pass){
 //   bcrypt.compare(req.body.password,"$2a$10$JA/Vy7.byewKZSbdo.j9ueahRKhpRd3/BQl9CYsJaifqJiXaNl7CG",function(err,pass){
@@ -70,13 +67,14 @@ router.post('/:script', function(req, res, next) {
 //      return;
 //    }
     if(req.session.signedin){
-      exec(__dirname+"/../scripts/"+req.params.script, function(err, stdout, stderr) {
-        console.log(err,stdout,stderr);
+      exec(__dirname+"/../scripts/"+req.params.script+" "+req.body.params, function(err, stdout, stderr) {
+        console.log("err: ",err,"stdout: ",stdout,"stderr: ",stderr);
         if(err){
           res.send("<a href='/'>< Back</a> <b>Program Error:</b> "+err.toString());
           return;
         }
-        res.send("<a href='/'>< Back</a> <b>Program Output:</b> <div style='white-space:pre-line>'"+stdout+"</div>");
+        //res.send("<a href='/'>< Back</a> <b>Program Output:</b> <div style='white-space:pre-line>'"+stdout+stderr+"</div>");
+        res.render("programOutput", {output: stdout});
       });
     }else{
       res.send("<a href='/'>< Back</a> <b>Permission Denied</b>");
--- RPi Server/scripts/hash.js
+++ RPi Server/scripts/hash.js
@@ -1,6 +1,4 @@
 #!/usr/local/bin/node
 var bcrypt = require("bcrypt");
 
-//console.log(bcrypt.hashSync(process.argv[2],10));
-console.log(bcrypt.hashSync("2",10));
-console.log("Hello World");
+console.log(bcrypt.hashSync(process.argv[2],10));
