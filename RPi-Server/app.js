b97bcecbb5f0903c840d68b227b15956c045fa20
Neil <neilbalch@gmail.com>
2017-05-21 17:15:39 -0700

    Added generic computer favicon to the server

--- RPi-Server/app.js
+++ RPi-Server/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var favicon = require('serve-favicon');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -24,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 app.use(session({
   saveUninitialized: false,
